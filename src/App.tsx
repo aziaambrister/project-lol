f9195032cdd95982fbaad3ca6d26fd98a800f8ec
aziaambrister <aziaambrister@yahoo.com>
2025-06-20 08:32:16 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,53 +6,67 @@ import CharacterSelect from './components/CharacterSelect';
 import GameWorld from './components/GameWorld';
 
 function App() {
-const { state } = useGame();
-const [gameState, setGameState] = useState<'welcome' | 'start' | 'character-select' | 'playing'>('welcome');
+  const { state } = useGame();
+  const [gameState, setGameState] = useState<'welcome' | 'start' | 'character-select' | 'playing'>('welcome');
 
-const handleEnterGame = () => {
-console.log('handleEnterGame called - transitioning to start screen');
-setGameState('start');
-};
+  const handleEnterGame = () => {
+    console.log('handleEnterGame called - transitioning to start screen');
+    setGameState('start');
+  };
 
-const handleStartGame = () => {
-console.log('handleStartGame called - transitioning to character-select');
-setGameState('character-select');
-};
+  const handleStartGame = () => {
+    console.log('handleStartGame called - transitioning to character-select');
+    setGameState('character-select');
+  };
 
-const handleCharacterSelected = () => {
-console.log('handleCharacterSelected called - transitioning to playing');
-setGameState('playing');
-};
+  const handleCharacterSelected = () => {
+    console.log('handleCharacterSelected called - transitioning to playing');
+    setGameState('playing');
+  };
 
-console.log('Current game state:', gameState);
+  console.log('Current game state:', gameState);
 
-return (
-<div className="min-h-screen bg-gray-100 relative">
-{gameState === 'welcome' && (
-<div className="absolute inset-0 z-10">
-<WelcomePage onEnter={handleEnterGame} />
-</div>
-)}
+  return (
+    <div className="min-h-screen bg-gray-100 relative">
+      {gameState === 'welcome' && (
+        <div className="absolute inset-0 z-10">
+          <WelcomePage onEnter={handleEnterGame} />
+        </div>
+      )}
 
-{gameState === 'start' && (
-<div className="absolute inset-0 z-10">
-<StartScreen onStart={handleStartGame} />
-</div>
-)}
+      {gameState === 'start' && (
+        <div className="absolute inset-0 z-10">
+          <StartScreen onStart={handleStartGame} />
+        </div>
+      )}
 
-{gameState === 'character-select' && (
-<div className="absolute inset-0 z-10">
-<CharacterSelect onSelectComplete={handleCharacterSelected} />
-</div>
-)}
+      {gameState === 'character-select' && (
+        <div className="absolute inset-0 z-10">
+          <CharacterSelect onSelectComplete={handleCharacterSelected} />
+        </div>
+      )}
 
-{gameState === 'playing' && (
-<div className="absolute inset-0 z-10">
-<GameWorld />
-</div>
-)}
-</div>
-);
+      {gameState === 'playing' && (
+        <div className="absolute inset-0 z-10">
+          {/* Character image added here for testing */}
+          <img
+            src="/imgs_png/ninja.png"
+            alt="Ninja"
+            style={{
+              position: 'absolute',
+              left: '50%',
+              top: '50%',
+              transform: 'translate(-50%, -50%)',
+              width: '100px',
+              height: '100px',
+              zIndex: 20,
+            }}
+          />
+          <GameWorld />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
