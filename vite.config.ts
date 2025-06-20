b4e80a185fbc479e55c56793ee22fd0fc1bfe85a
aziaambrister <aziaambrister@yahoo.com>
2025-06-20 09:55:05 -0400

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
-    port: 3001,
-    strictPort: true,
+    port: 5173,      // Use Vite's default port
+    strictPort: true // Fail if 5173 is taken, don't try 5174, etc.
   },
 });
